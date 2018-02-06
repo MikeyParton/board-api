1af5488fc023759e210d46e549aca8e63dd91c0e
Mike Parton <mikeparton@live.com.au>
2018-02-06 20:01:29 -0300

    Remove expiration on tokens because it was annoying

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -1,18 +1,11 @@
 class JsonWebToken
-  def self.encode(payload, exp = 2.hours.from_now)
-    # set token expiration time
-    payload[:exp] = exp.to_i
-
-     # this encodes the user data(payload) with our secret key
+  def self.encode(payload)
     JWT.encode(payload, Rails.application.secrets.secret_key_base)
   end
 
   def self.decode(token)
-    #decodes the token to get user data (payload)
     body = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
     HashWithIndifferentAccess.new body
-
-  # raise custom error to be handled by custom handler
   rescue JWT::ExpiredSignature, JWT::VerificationError => e
     raise ExceptionHandler::ExpiredSignature, e.message
   rescue JWT::DecodeError, JWT::VerificationError => e
