1dc259df30478798cc0500580435e43e337891fe
Mike Parton <mikeparton@live.com.au>
2018-02-25 17:31:13 -0300

    Controller action for updating checklist items

--- app/controllers/checklist_items_controller.rb
+++ app/controllers/checklist_items_controller.rb
@@ -1,6 +1,14 @@
 class ChecklistItemsController < AuthorizedController
   before_action :find_checklist_item
 
+  def update
+    if @checklist_item.update(permitted_params.checklist_item)
+      render json: { checklist_items: [ChecklistItemSerializer.new(@checklist_item)] }, status: 200
+    else
+      render json: { errors: @checklist_item.errors }, status: 422
+    end
+  end
+
   def complete
     @checklist_item.completed_by = current_user
     if @checklist_item.complete!
